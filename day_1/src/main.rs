69c2c396723b7f8f1a4bc4e91e95a63229873ba7
Rein van 't Veer <reinvantveer@gmail.com>
2020-12-02 19:34:52 +0100

    Completion of day 1 phase 2: triple-entry combo

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -14,11 +14,31 @@ fn main() {
         .map(|entry| entry.parse::<usize>().unwrap())
         .collect();
 
-    for entry_idx in 1..entries.len() {
-        for inner_entry_idx in 1..entries.len() {
-            if entries[entry_idx] + entries[inner_entry_idx] == flag_hint {
-                println!("Flag found: {}", entries[entry_idx] * entries[inner_entry_idx]);
-                println!("Entries {} {}", entries[entry_idx], entries[inner_entry_idx]);
+    double_entry_flag(flag_hint, entries.clone());
+    triple_entry_flag(flag_hint, entries)
+}
+
+fn double_entry_flag(flag_hint: usize, entries: Vec<usize>) {
+    for outer_entry_idx in 1..entries.len() {
+        for inner_entry_idx in outer_entry_idx..entries.len() {
+            if entries[outer_entry_idx] + entries[inner_entry_idx] == flag_hint {
+                println!("Double entry flag found: {}", entries[outer_entry_idx] * entries[inner_entry_idx]);
+                println!("Entries {} {}", entries[outer_entry_idx], entries[inner_entry_idx]);
+            }
+        }
+    }
+}
+
+fn triple_entry_flag(flag_hint: usize, entries: Vec<usize>) {
+    for first_entry_idx in 1..entries.len() {
+        for second_entry_idx in first_entry_idx..entries.len() {
+            for third_entry_idx in second_entry_idx..entries.len() {
+                if entries[first_entry_idx]
+                    + entries[second_entry_idx]
+                    + entries[third_entry_idx] == flag_hint {
+                        println!("Triple entry flag found: {}", entries[first_entry_idx] * entries[second_entry_idx] * entries[third_entry_idx]);
+                        println!("Entries {} {} {}", entries[first_entry_idx], entries[second_entry_idx], entries[third_entry_idx]);
+                    }
             }
         }
     }
