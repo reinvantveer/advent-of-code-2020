247a85d9a22acd34c04c0ca0e17b0f0a251729aa
Rein van 't Veer <reinvantveer@gmail.com>
2020-12-03 19:53:33 +0100

    there on phase 1 and 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -12,6 +12,19 @@ fn main() {
         .map(|entry| entry_to_tuple(entry))
         .collect();
 
+    let mut failed = 0;
+    let mut succeeded = 0;
+
+    occurrence_validation(entries.clone(), &mut failed, &mut succeeded);
+    println!("There were {} succeeded occurrence-valid entries and {} failed", succeeded, failed);
+
+    failed = 0;
+    succeeded = 0;
+    position_validation(entries, &mut failed, &mut succeeded);
+    println!("There were {} succeeded position-valid entries and {} failed", succeeded, failed);
+}
+
+fn occurrence_validation(entries: Vec<(String, String, String)>, failed: &mut i32, succeeded: &mut i32) {
     for entry in entries {
         let occurrence_range: Vec<usize> = entry.0
             .split("-")
@@ -28,13 +41,41 @@ fn main() {
             .filter(|character| character.to_string() == char_to_check)
             .collect();
 
-        if char_occurrences.len() < minimum {
-            println!("{} has less than {} required {}'s: {}",
-                     &entry.2, minimum, &char_to_check, char_occurrences.len())
+        if char_occurrences.len() < minimum || char_occurrences.len() > maximum {
+            // println!("{} has less than {} required {}'s: {}",
+            //          &entry.2, minimum, &char_to_check, char_occurrences.len())
+            *failed += 1;
+        } else {
+            *succeeded += 1;
         }
     }
+}
 
-    println!("Hello, world!");
+fn position_validation(entries: Vec<(String, String, String)>, failed: &mut i32, succeeded: &mut i32) {
+    for entry in entries {
+        let positions: Vec<usize> = entry.0
+            .split("-")
+            .map(|min_or_max| min_or_max.parse().unwrap())
+            .collect();
+
+        let index_1: usize = positions[0] - 1;  // We need to subtract one to make it a 0-indexed idx
+        let index_2: usize = positions[1] - 1;
+
+        let char_to_check = entry.1
+            .chars()
+            .next()
+            .unwrap(); // First character in second entry
+
+        let chars: Vec<char> = entry.2.chars().collect();
+
+        if (chars[index_1] == char_to_check) ^ (chars[index_2] == char_to_check) {
+            // println!("{} has less than {} required {}'s: {}",
+            //          &entry.2, minimum, &char_to_check, char_occurrences.len())
+            *succeeded += 1;
+        } else {
+            *failed += 1;
+        }
+    }
 }
 
 fn entry_to_tuple(entry: &str) -> (String, String, String) {
