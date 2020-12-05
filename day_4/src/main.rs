1cd864b98d0a964dab78d7799c0af8abd6f2a5ad
Rein van 't Veer <reinvantveer@gmail.com>
2020-12-05 15:54:50 +0100

    passport parser begin

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -2,9 +2,36 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    let lines: Vec<&str> = input
-        .lines()
+    let passports: Vec<&str> = input
+        // Assuming Unix-type line-feed line endings here!
+        .split(&"\n\n".to_string())
         .collect();
 
-    println!("Hello, world!");
+    println!("{} passports found", passports.len());
+
+    for passport in &passports {
+        validate_passport(passport);
+        break;
+    }
+}
+
+fn validate_passport(passport: &&str) {
+    let passport_joined = passport
+        .replace("\n", " "); // Get rid of newlines
+
+    let passport_keys: Vec<&str> = passport_joined
+        .split(" ")
+        .collect();  // convert to iterator over passport keys
+
+    println!("{} keys in passport {}", passport_keys.len(), passport_keys.join(" "));
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_add() {
+        let some_string = "aa\n\ncc".to_string();
+        let splits: Vec<&str> = some_string.split("\n\n").collect();
+        assert_eq!(splits, vec!["aa", "cc"]);
+    }
 }
