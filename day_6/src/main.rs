182bee205b52d7b11f9e44118e5a57f50e1e4373
Rein van 't Veer <reinvantveer@gmail.com>
2020-12-07 21:53:25 +0100

    beginning of day 6

---
+++ day_6/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs;
+
+fn main() {
+    read_lines("input.txt");
+
+    println!("Hello, world!");
+}
+
+fn read_lines(path: &str) -> Vec<String> {
+    let input = fs::read_to_string(path)
+        .unwrap();
+
+    let lines: Vec<String> = input.lines().map(String::from).collect();
+    lines
+}
+
+fn get_group_answers(lines: &Vec<String>) -> Vec<String> {
+    let mut grouped: Vec<String> = Vec::new();
+
+    grouped
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{read_lines, get_group_answers};
+
+    #[test]
+    fn figure_out_correct_example_answer() {
+        let lines = read_lines("example.txt");
+        let group_answers = get_group_answers(&lines);
+        assert_eq!(group_answers[0], "abc");
+        assert_eq!(group_answers[1], "abc")
+    }
+}
