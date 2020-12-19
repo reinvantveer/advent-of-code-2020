199b3317e782974058bfafd21eb3ad632d43b8d4
reinvantveer <reinvantveer@gmail.com>
2020-12-19 19:56:43 +0100

    beginnings of turing machine

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashSet;
 
 fn main() {
     println!("Hello, world!");
@@ -34,17 +35,44 @@ fn parse_instruction(line: &String) -> Instruction {
     }
 }
 
+fn run_code_until_already_executed(instructions: Vec<Instruction>) -> isize {
+    let mut already_executed = HashSet::new();
+    let mut program_counter = 0;
+    let mut accumulator = 0 as isize;
+
+    loop {
+        if already_executed.contains(&program_counter) {
+            println!("End of loop detected: {} already visited", &program_counter);
+            break;
+        }
+        let instruction = &instructions[program_counter];
+        already_executed.insert(program_counter);
+        program_counter += 1;
+    }
+
+    accumulator
+}
 
 #[cfg(test)]
 mod test {
-    use crate::{read_lines, parse_instructions};
+    use crate::{read_lines, parse_instructions, run_code_until_already_executed};
 
     #[test]
-    fn test_example_code() {
+    fn test_example_code_parser() {
         let lines = read_lines("example.txt");
         let instructions = parse_instructions(&lines);
 
         assert_eq!(instructions[0].operation, "nop");
         assert_eq!(instructions[5].amount, -99);
     }
+
+    #[test]
+    fn test_once_execution() {
+        let lines = read_lines("example.txt");
+        let instructions = parse_instructions(&lines);
+
+        let accumulator_value = run_code_until_already_executed(instructions);
+        assert_eq!(accumulator_value, 5)
+    }
+
 }
