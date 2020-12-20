abbcdddee937a13896a837fd370d60435488092c
reinvantveer <reinvantveer@gmail.com>
2020-12-20 19:31:28 +0100

    bootstrap contiguous sum finder

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -78,9 +78,16 @@ fn partial_cartesian<T: Clone>(a: Vec<Vec<T>>, b: &[T]) -> Vec<Vec<T>> {
     }).collect()
 }
 
+fn get_contiguous_sum_range(numbers: &Vec<usize>, sum: usize) -> (usize, usize) {
+    for (num_idx, num) in numbers.iter().enumerate() {
+
+    }
+    (0, 0)
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{read_lines, lines_to_numbers, find_first_wrong_number};
+    use crate::{read_lines, lines_to_numbers, find_first_wrong_number, get_contiguous_sum_range};
 
     #[test]
     fn test_first_wrong_number() {
@@ -88,5 +95,14 @@ mod test {
         let numbers = lines_to_numbers(&lines);
         let wrong = find_first_wrong_number(numbers, 5);
         assert_eq!(wrong, 127)
+    }
+
+    #[test]
+    fn test_contiguous_sum() {
+        let lines = read_lines("example.txt");
+        let numbers = lines_to_numbers(&lines);
+        let (first_idx, last_idx) = get_contiguous_sum_range(&numbers, 127);
+        assert_eq!(first_idx, 2);
+        assert_eq!(last_idx, 5)
     }
 }
