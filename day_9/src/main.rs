10146cc4e6b49266cff596d5ebd158d02f6b810c
reinvantveer <reinvantveer@gmail.com>
2020-12-20 19:13:38 +0100

    solve day 9 part 1

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -1,7 +1,10 @@
 use std::fs;
 
 fn main() {
-    println!("Hello, world!");
+    let lines = read_lines("input.txt");
+    let numbers = lines_to_numbers(&lines);
+    let first_wrong_number = find_first_wrong_number(numbers, 25);
+    println!("First wrong number: {}", first_wrong_number);
 }
 
 fn read_lines(path: &str) -> Vec<String> {
@@ -21,8 +24,58 @@ fn lines_to_numbers(lines: &Vec<String>) -> Vec<usize> {
         .collect()
 }
 
-fn find_first_wrong_number(numbers: Vec<usize>) -> usize {
-    5
+fn find_first_wrong_number<'a>(
+    numbers: Vec<usize>,
+    preamble_len: usize
+) -> usize {
+    let verifiable_numbers = numbers[preamble_len..].iter();
+
+    for (idx, num_to_check) in verifiable_numbers.enumerate() {
+        // Preamble start = idx because at idx = 0: preamble_start = 0
+        let preamble_stop = idx + preamble_len + 1;  // Cuz at idx = 0 preamble_stop = 6
+        let preamble = &numbers[idx..preamble_stop];
+
+        let cartesian = cartesian_product(&[&preamble, &preamble]);
+
+        let sums: Vec<usize> = cartesian
+            .iter()
+            .filter(|combi| combi[0] != combi[1])
+            .map(|combi| combi[0] + combi[1])
+            .collect();
+
+        if !sums.contains(num_to_check) {
+            println!("{:?}", num_to_check);
+            return *num_to_check;
+        }
+    }
+    unreachable!("No errors found");
+}
+
+// Thanks kylewlacy: https://gist.github.com/kylewlacy/115965b40e02a3325558
+fn cartesian_product<T: Clone>(lists: &[&[T]]) -> Vec<Vec<T>> {
+    match lists.split_first() {
+        Some((first, rest)) => {
+            let init: Vec<Vec<T>> = first.iter().cloned().map(|n| vec![n]).collect();
+
+            rest.iter().cloned().fold(init, |vec, list| {
+                partial_cartesian(vec, list)
+            })
+        },
+        None => {
+            vec![]
+        }
+    }
+}
+
+// Thanks kylewlacy: https://gist.github.com/kylewlacy/115965b40e02a3325558
+fn partial_cartesian<T: Clone>(a: Vec<Vec<T>>, b: &[T]) -> Vec<Vec<T>> {
+    a.into_iter().flat_map(|xs| {
+        b.iter().cloned().map(|y| {
+            let mut vec = xs.clone();
+            vec.push(y);
+            vec
+        }).collect::<Vec<_>>()
+    }).collect()
 }
 
 #[cfg(test)]
@@ -33,7 +86,7 @@ mod test {
     fn test_first_wrong_number() {
         let lines = read_lines("example.txt");
         let numbers = lines_to_numbers(&lines);
-        let wrong = find_first_wrong_number(numbers);
+        let wrong = find_first_wrong_number(numbers, 5);
         assert_eq!(wrong, 127)
     }
 }
